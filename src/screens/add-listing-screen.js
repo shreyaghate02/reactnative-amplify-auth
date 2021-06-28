c71b89149d761c89f74adbafb24afe92e755b9e1
Shreya Ghate <shreya.ghate.sg@gmail.com>
2021-06-28 15:01:06 +0530

    Auth flow setup conifgured

---
+++ src/screens/add-listing-screen.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {SafeAreaView, StatusBar, Text} from 'react-native';
+
+const AddListingScreen = props => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <Text>Add Listing Screen</Text>
+      </SafeAreaView>
+    </>
+  );
+};
+
+export default AddListingScreen;
